63398d7dac255311a0bd5b89533e5bf8ac611bdb
Alex Bowyer <alex.bowyer@gmail.com>
2016-09-01 00:14:13 +0100

    Improved testing - intervention after a sequence of classifications

--- tests/test_volcrowe_experiment_seq.rb
+++ tests/test_volcrowe_experiment_seq.rb
@@ -42,30 +42,53 @@ class TestVolcroweExperimentSeq < Test::Unit::TestCase
         nextEvent = session_plan[seq]
         current_session_history = ["classification:#{@@CLASSIFICATION_IDS[classification_id_index]}"]
         assert_equal(current_session_history,data["message"]["current_session_history"],"After #{step} classifications, expected current session history to be updated.")
-        #binding.pry
-        #
-        #step = 1
-        #binding.pry
-        #while nextEvent==@@CLASSIFICATION_MARKER
-        #  step += 1
-        #  classification_id_index += 1
-        #  uri2 = URI.parse("#{@@SERVER_URL}/experiment/CometHuntersVolcroweExperiment1/user/#{@@TEST_STATEMENTS_USER_ID}/session/#{@@FIRST_SESSION_ID}/classification/#{@@CLASSIFICATION_IDS[classification_id_index]}")
-        #  http2 = Net::HTTP.new(uri2.host, uri2.port)
-        #  request2 = Net::HTTP::Post.new(uri2.request_uri)
-        #  response2 = http2.request(request2)
-        #  assert_equal("200",response2.code)
-        #  data2 = JSON.parse(response2.body)
-        #  assert_equal(@@FIRST_SESSION_ID,data2["message"]["current_session_id"], "After #{step} classifications, expected session ID not to change.")
-        #  seq += 1
-        #  assert_equal(seq,data2["message"]["seq_of_next_event"],"After #{step} classifications, expected session plan pointer to advance.")
-        #  assert_equal(session_plan,data2["message"]["current_session_plan"],"After #{step} classifications, expected session plan not to change.")
-        #  current_session_history.push "classification:#{@@CLASSIFICATION_IDS[classification_id_index]}"
-        #  assert_equal(current_session_history,data2["message"]["current_session_history"],"After #{step} classifications, expected current session history to be updated.")
-        #  nextEvent = session_plan[seq]
-        #end
-        #assert_not_equal(@@CLASSIFICATION_MARKER,session_plan[seq],"After #{step} classifications, verifying test logic: Expected the next event in the session plan to be an intervention.")
-        ## now post the intervention.
-        ## and check everything.
+        step = 1
+        while nextEvent==@@CLASSIFICATION_MARKER
+          step += 1
+          classification_id_index += 1
+          uri = URI.parse("#{@@SERVER_URL}/experiment/CometHuntersVolcroweExperiment1/user/#{@@TEST_STATEMENTS_USER_ID}/session/#{@@FIRST_SESSION_ID}/classification/#{@@CLASSIFICATION_IDS[classification_id_index]}")
+          http = Net::HTTP.new(uri.host, uri.port)
+          request = Net::HTTP::Post.new(uri.request_uri)
+          response = http.request(request)
+          assert_equal("200",response.code)
+          data = JSON.parse(response.body)
+          assert_equal(@@FIRST_SESSION_ID,data["message"]["current_session_id"], "After #{step} classifications, expected session ID not to change.")
+          seq += 1
+          assert_equal(seq,data["message"]["seq_of_next_event"],"After #{step} classifications, expected session plan pointer to advance.")
+          assert_equal(session_plan,data["message"]["current_session_plan"],"After #{step} classifications, expected session plan not to change.")
+          current_session_history.push "classification:#{@@CLASSIFICATION_IDS[classification_id_index]}"
+          assert_equal(current_session_history,data["message"]["current_session_history"],"After #{step} classifications, expected current session history to be updated.")
+          nextEvent = session_plan[seq]
+        end
+        assert_not_equal(@@CLASSIFICATION_MARKER,session_plan[seq],"After #{step} classifications, verifying test logic: Expected the next event in the session plan to be an intervention.")
+        # now post the intervention.
+        intervention_to_post = session_plan[seq]
+        uri = URI.parse("#{@@SERVER_URL}/experiment/CometHuntersVolcroweExperiment1/user/#{@@TEST_STATEMENTS_USER_ID}/session/#{@@FIRST_SESSION_ID}/intervention/#{intervention_to_post}")
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        response = http.request(request)
+        assert_equal("200",response.code)
+        seq += 1
+        current_session_history.push "intervention:#{intervention_to_post}"
+        data = JSON.parse(response.body)
+        # and check everything is as expected
+        assert_equal(CometHuntersVolcroweExperiment1::getExperimentName,data["message"]["experiment_name"],"Wrong experiment name.")
+        assert_equal(CometHuntersVolcroweExperiment1::getStatementsCohort,data["message"]["cohort"],"Wrong cohort.")
+        assert_equal(@@TEST_STATEMENTS_USER_ID,data["message"]["user_id"],"Wrong user ID.")
+        assert(data["message"]["active"]==true,"Expected participant to be active.")
+        assert(data["message"]["excluded"]==false,"Expected participant not to be excluded.")
+        assert_nil(data["message"]["excluded_reason"],"Expected no exclusion reason")
+        assert_equal(14,data["message"]["interventions_available"].length,"Expected 14 available interventions.")
+        assert(!data["message"]["interventions_available"].include?(intervention_to_post),"After #{step} classifications and the #{intervention_to_post} intervention, expected that intervention no longer to be marked available.")
+        assert_equal(1,data["message"]["interventions_seen"].length,"Expected 1 seen intervention.")
+        assert_equal(intervention_to_post,data["message"]["interventions_seen"][0],"After #{step} classifications and the #{intervention_to_post} intervention, expected that intervention marked seen.")
+        assert_equal(Hash.new, data["message"]["original_session_plans"],"After #{step} classifications and the #{intervention_to_post} intervention, expected no original session plans.")
+        assert_equal(Hash.new, data["message"]["session_histories"],"After #{step} classifications and the #{intervention_to_post} intervention, expected no session histories.")
+        assert_equal(@@FIRST_SESSION_ID,data["message"]["current_session_id"], "After #{step} classifications and the #{intervention_to_post} intervention, expected session ID not to change.")
+        assert_equal(current_session_history,data["message"]["current_session_history"],"After #{step} classifications and the #{intervention_to_post} intervention, expected current session history to be updated.")
+        assert_equal(session_plan,data["message"]["current_session_plan"],"After #{step} classifications and the #{intervention_to_post} intervention, expected session plan not to change.")
+        assert_equal(seq,data["message"]["seq_of_next_event"],"After #{step} classifications and the #{intervention_to_post} intervention, expected session plan pointer to advance.")
+        # now one more classification
       ensure
         deleteUser(@@TEST_STATEMENTS_USER_ID)
       end
@@ -82,3 +105,16 @@ class TestVolcroweExperimentSeq < Test::Unit::TestCase
 ##
 
 end
+
+## tests TODO:
+
+# new session
+# reset session
+# full end to end sequence of an experimental session
+# end of experiment (empty/inactive)
+# past intervention when not expected
+# absent intervention when not expected
+# future intervention when not expected
+# classification when intervention was expected
+# classification after end of experiment
+# intervention after end of experiment
